3272ef8df671c9a8fa2fd15b445d49cbddd42f3e
Jose Sebastian <106174138+4kiras@users.noreply.github.com>
2023-04-26 20:55:44 -0500

    2.0

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ console.log("hola mundo");
 function sumar(a, b) {
     return a + b;
 }
+//funcion de fibonacci
+function fibonacci(n) {
+    if (n <= 1)
+        return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
